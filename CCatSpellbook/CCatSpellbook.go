e84896d1d1cf072480f431874a0de12214988f80
CCat <61354181+CTathlete@users.noreply.github.com>
2023-12-07 22:43:49 +0200

    Added my spellbook.

---
+++ CCatSpellbook/CCatSpellbook.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"os"
+)
+
+func CheckErr(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func copy(src string, dst string) {
+	// Reads src and writes it to dst.
+	data, err := os.ReadFile(src)
+	CheckErr(err)
+	err = os.WriteFile(dst, data, 0644)
+	CheckErr(err)
+}
---
+++ CCatSpellbook/go.mod
@@ -0,0 +1,3 @@
+module CCatSpellbook
+
+go 1.21.5
